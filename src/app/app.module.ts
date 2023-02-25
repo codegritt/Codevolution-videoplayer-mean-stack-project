738832a634d02739d770f5d923bb7dd49b3d8bf2
gokul <gokulchandan18@gmail.com>
2023-02-25 16:57:51 +0530

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { VideoCenterComponent } from './video-center/video-center.component';
+import { VideoDetailComponent } from './video-detail/video-detail.component';
+import { VideoListComponent } from './video-list/video-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VideoCenterComponent,
+    VideoDetailComponent,
+    VideoListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/video-center/video-center.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-center',
+  templateUrl: './video-center.component.html',
+  styleUrls: ['./video-center.component.css']
+})
+export class VideoCenterComponent {
+
+}
